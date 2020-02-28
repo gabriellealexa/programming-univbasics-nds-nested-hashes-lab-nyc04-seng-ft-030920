8841af942e35a60472ef842514ee5825693104dd
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-28 06:47:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,13 +18,12 @@ end
 
 def literal_aoh
   aoh = [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
+    DON_G.sort,
+    JOELLE_VD.sort,
+    PAT_M.sort,
+    KATE_G.sort,
+    BRUCE_G.sort
     ]
-    aoh.sorted
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
