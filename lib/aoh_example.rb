bfbddd9f9cb938037028389bb2e19c073bc8a359
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-28 06:47:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,6 +24,7 @@ def literal_aoh
     KATE_G.sort,
     BRUCE_G.sort
     ]
+    aoh.sort 
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
